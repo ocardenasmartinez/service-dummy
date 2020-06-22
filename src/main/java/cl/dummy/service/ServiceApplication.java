3fdf7dd9cdca7e12ea9a7158038051d277d5a721
Octavio <ocardenasmartinez@gmail.com>
2020-06-21 21:50:57 -0400

    add eureka

--- src/main/java/cl/dummy/service/ServiceApplication.java
+++ src/main/java/cl/dummy/service/ServiceApplication.java
@@ -2,8 +2,10 @@ package cl.dummy.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceApplication {
 
 	public static void main(String[] args) {
